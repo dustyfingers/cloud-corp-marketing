9377ee65d63c70de89c65cc665fec5f7b801e92f
lou <dustyfingers01257@gmail.com>
2024-10-24 11:00:38 -0600

    move header and font into src directory

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,16 @@
 import type { Metadata } from "next";
-import Link from "next/link";
 import localFont from "next/font/local";
 
+import Header from "@/components/Header";
 import "./globals.css";
 
 const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
+  src: "../fonts/GeistVF.woff",
   variable: "--font-geist-sans",
   weight: "100 900",
 });
 const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
+  src: "../fonts/GeistMonoVF.woff",
   variable: "--font-geist-mono",
   weight: "100 900",
 });
@@ -30,12 +30,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <div>
-          <Link href="/">Home</Link>
-          <Link href="/performance">Performance</Link>
-          <Link href="/scale">Scale</Link>
-          <Link href="/reliability">Reliability</Link>
-        </div>
+        <Header />
         {children}
       </body>
     </html>
