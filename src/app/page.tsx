a2397563d1c8d7474cde15ea7da67979e6188c14
lou <dustyfingers01257@gmail.com>
2024-10-24 11:20:35 -0600

    add image to home page, learn more about nexts image component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,19 @@
+import Image from "next/image";
+
+import homeImage from "public/home.jpg";
+
 export default function Home() {
-  return <div>Home Page</div>;
+  return (
+    <div>
+      Home Page
+      <div className="absolute -z-10 inset-0">
+        <Image
+          src={homeImage}
+          alt="car factory"
+          fill
+          className="object-cover"
+        />
+      </div>
+    </div>
+  );
 }
